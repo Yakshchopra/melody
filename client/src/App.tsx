f6c3749c8f4f3a1f9d55ee71c114f1730e835818
Yakshchopra <yakshchopra@gmail.com>
2021-03-31 19:51:20 +0530

    🎉 tailwind-init

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1 className='text-red-400'>Hello from tailwind</h1>
+    </div>
+  );
+}
+
+export default App;
