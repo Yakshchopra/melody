b0e2a9d77dc41cbfe6edc94c19e007a71b75c329
Piyush Goel <piyushgoel2008@gmail.com>
2021-05-22 15:25:03 +0530

    SideNavBar added

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,13 +1,15 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        dark: '#1D1D27',
-        primary: '#7D4BCD',
-        bglight: '#292938',
-        input: '#37374A',
+        dark: "#1D1D27",
+        primary: "#7D4BCD",
+        bglight: "#292938",
+        input: "#37374A",
+        bgselected: "#1D1D27",
+        purpleselected: "#6132AF",
       },
     },
   },
