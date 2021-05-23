7727ac1bd6f4b12b7132f6677d6a9158ed09850c
Yakshchopra <yakshchopra@gmail.com>
2021-05-23 09:46:30 +0530

    ✨ added store

---
+++ client/src/store/reducer.ts
@@ -0,0 +1,23 @@
+export const initialState = {
+  user: null,
+  recents: null,
+};
+
+function reducer(state: any, action: any) {
+  switch (action.type) {
+    case 'SET_USER':
+      return {
+        ...state,
+        user: action.user,
+      };
+    case 'SET_RECENT':
+      return {
+        ...state,
+        recents: action.recents,
+      };
+
+    default:
+      return state;
+  }
+}
+export default reducer;
